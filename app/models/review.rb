362031e48c450ba0afe86cf253a315ce297cbe87
KateDay <khummle@gmail.com>
2019-06-15 15:38:41 -0600

    Review: stronger validation- rating of 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ActiveRecord::Base
   belongs_to :user
 
   validates :description, presence: true
-  validates :rating, presence:true
+  validates :rating, numericality: { only_integer: true, :in => 1..5 }, presence: true
 end
