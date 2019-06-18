ab98d60dd8e324e8c080cd5540da4653596c3d64
KateDay <khummle@gmail.com>
2019-06-18 14:41:53 -0600

    user email verification uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
+  validates_presence_of :password_confirmation
   has_secure_password
 
   has_many :reviews
+
+  validates :email, uniqueness: { case_sensitive: false }
+  validates_presence_of :email, :first_name, :last_name
+  validates :password, length: { minimum: 3 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+    subject{
+      described_class.new(
+        email: "this@test.com",
+        first_name: "Dude",
+        last_name: "Man",
+        password: "testpass",
+        password_confirmation: "testpass"
+      )
+    }
+
+    it "should have a unique email" do
+      subject2 = User.new(
+        email: "this@test.com", 
+        first_name: "Broseph", 
+        last_name: "Radington", 
+        password: "thing", 
+        password_confirmation:"thing"
+      )
+      subject.save
+      subject2.save
+      expect(subject).to be_valid
+      expect(subject2).to_not be_valid
+      expect(subject2.errors[:email]).to include("has already been taken")
+    end
+
+    it "should have all valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "should have a matching password and password confirmation" do
+      subject.password_confirmation = "tiestspass"
+      expect(subject).to_not be_valid
+    end
+  end
 end
